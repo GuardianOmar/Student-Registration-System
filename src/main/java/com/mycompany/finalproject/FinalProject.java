d3d6be3dd13ee3d1053903f97bbcfe815bbba182
Falcon TECH <Falcon TECH@DESKTOP-LQJTGGK>
2023-05-02 22:10:50 +0200

    comment

--- src/main/java/com/mycompany/finalproject/FinalProject.java
+++ src/main/java/com/mycompany/finalproject/FinalProject.java
@@ -3,6 +3,13 @@ package com.mycompany.finalproject;
 public class FinalProject {
 
     public static void main(String[] args) {
-        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new inter_face().setVisible(true);
+            }
+        });
+        //test
+        student s1=new student("",1,"",1,1,"","","","","","","","");
+        s1.printdata();
     }
 }
